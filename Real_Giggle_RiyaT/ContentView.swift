4d8c792982afebab4946052015f41d24aaed0745
riyatadinada <27rtadinada@castilleja.org>
2026-03-16 13:39:17 -0700

    Initial Commit

---
+++ Real_Giggle_RiyaT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real_Giggle_RiyaT
+//
+//  Created by Riya Tadinada on 3/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Real_Giggle_RiyaT/Real_Giggle_RiyaTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Giggle_RiyaTApp.swift
+//  Real_Giggle_RiyaT
+//
+//  Created by Riya Tadinada on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Real_Giggle_RiyaTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
